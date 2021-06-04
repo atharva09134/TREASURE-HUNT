dca3e73e3c6cfd8db58e916b20f508d229ef4036
Atharva Vadnere <75987472+atharva09134@users.noreply.github.com>
2021-06-04 17:49:05 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "1";
+const accessCode2 = "2";
+const accessCode3 = "3";
+
+function clues() {
+    
+    fill("red")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("orange")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("red")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("orange")
+    text("Hint: Performs a particular task !!", 700,170)
+
+    fill("red")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("orange")
+    text("Hint: Stores all information !!", 100,270)
+
+}
